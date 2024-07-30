9d457bcd28150e2a236991c73cdd7cb688de7c59
Josh Wilcox <joshuawilcox63@gmail.com>
2024-07-30 08:20:21 -0500

    Push all lesson learning
    pushed all my learnings to github

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Celsius) {
+  fahrenheit = ((Celsius - 32) * (5 / 9))
+    function round(value, precision){
+      let multiplier = Math.pow(10, precision || 0);
+      return Math.round(value * multiplier)/ multiplier;
+
+    }
+    return round(fahrenheit,1)
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  celsius = ((fahrenheit *(9 / 5) + 32))
+    function round(value, precision){
+      let multiplier = Math.pow(10, precision || 0);
+      return Math.round(value * multiplier)/ multiplier;
+
+    }
+    return round(celsius,1)
 };
 
 // Do not edit below this line
